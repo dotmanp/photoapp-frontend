609c4c1108e2702384f5ed0d3b638fa34ee6c282
Godstime2020 <godstimeinstein@gmail.com>
2025-05-08 08:05:42 +0100

    deployment bug fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist',
+  },
   base: './', // relative path
   plugins: [react()],
+  server: {
+    historyApiFallback: true
+  }
 })
